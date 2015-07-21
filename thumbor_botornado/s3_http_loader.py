3f2ce099829770833867e2c7e4cd2e095f54f40e
Sebastian Eichner <mail@sebastian-eichner.de>
2015-07-21 14:08:33 +0200

    wip

--- thumbor_botornado/s3_http_loader.py
+++ thumbor_botornado/s3_http_loader.py
@@ -11,14 +11,17 @@ LIBRATO_API = librato.connect(os.environ.get('LIBRATO_USER'), os.environ.get('LI
 
 @return_future
 def load(context, url, callback):
+    logger.debug("*** LOADER.load" )
     start = datetime.datetime.now()
     if HTTP_RE.match(url):
         method = 'http'
         HttpLoader.load(context, url, callback)
     else:
         method = 's3'
+        logger.debug("*** s3")
         S3Loader.load(context, url, callback)
     finish = datetime.datetime.now()
     metric_name = "thumbor.loader.%s.request_time" % method
+    logger.debug("*** librato submit")
     LIBRATO_API.submit(metric_name, (finish - start).total_seconds() * 1000, description="original file request time (ms)")
 
